1ba1541961c8c52dde70298effd47a536a536148
Jorge Celaya <jcelaya775@gmail.com>
2024-12-08 22:57:50 -0600

    completions: pnpm

--- config.fish
+++ config.fish
@@ -491,8 +491,6 @@ set --export PATH $BUN_INSTALL/bin $PATH
 # pnpm
 set -gx PNPM_HOME /Users/jorge/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-    # pnpm end
-    # pnpm end
     se-gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
