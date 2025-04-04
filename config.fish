117ffa4b04a88b8328bcb76d050b1678cabce39f
Jorge Celaya <jcelaya775@gmail.com>
2025-04-03 19:53:18 -0500

    fish: add anaconda to path

--- config.fish
+++ config.fish
@@ -615,13 +615,13 @@ bind -M visual -m default y 'fish_clipboard_copy; commandline -f end-selection r
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/anaconda3/bin/conda
-    eval /opt/anaconda3/bin/conda "shell.fish" hook $argv | source
+if test -f /home/jorge/anaconda3/bin/conda
+    eval /home/jorge/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 else
-    if test -f "/opt/anaconda3/etc/fish/conf.d/conda.fish"
-        . "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+    if test -f "/home/jorge/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/home/jorge/anaconda3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH /opt/anaconda3/bin $PATH
+        set -x PATH "/home/jorge/anaconda3/bin" $PATH
     end
 end
 # <<< conda initialize <<<
